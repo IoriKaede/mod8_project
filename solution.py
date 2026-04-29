f15377caa6454579890856f8f419d34e90dd18a0
IoriKaede <paranoia170214@gmail.com>
2026-04-30 00:43:03 +0200

    tiny changes with comments inside. changed something with spread and it's finally working

--- solution.py
+++ solution.py
@@ -8,7 +8,7 @@ bid_queue = []
 ask_queue = []
 count = 0
 random.seed(42)
-
+T = 0
 
 class Limit_order:
     def __init__(self, order, ba, price, arrival_time):
@@ -38,10 +38,11 @@ class LOB:
         self.spread = 0
         self.length()
 
-        self.spread= 0
         self.spread_last_time = 0
-        self.spread_boolean=True
+        self.spread_check=False
         self.spread_integral = 0
+        self.spread_last_value =0
+        self.spread_total_time = 0
 
 
     def ba(self, order):
@@ -56,12 +57,12 @@ class LOB:
 
     def best_bid(self):
         if not bid_queue: #in case it is empty
-            return
+            return None
         return max(bid_queue,key=lambda order: order.price)
 
     def best_ask(self):
         if not ask_queue:
-            return
+            return None
         return min(ask_queue, key=lambda order: order.price)
 
     def queue_status(self, t):
@@ -72,11 +73,31 @@ class LOB:
         ba = self.best_ask()
         bb = self.best_bid()
         spread = {}
-        if not self.ask_length or not self.bid_length: #for the object is not an integer use not :)
-            spread[t] = "a"
+        if ba is None or bb is None: #for the object is not an integer use not :)
+            spread[t] = None #undefined
         else:
             spread[t] = ba.price - bb.price  #subtract the price of best ba not themselves
         return spread
+
+    def spread_object(self, t):  #same thing as above but make track of time
+        ba = self.best_ask()
+        bb = self.best_bid()
+
+        if self.spread_check:
+            dt = t - self.spread_last_time
+            self.spread_integral += self.spread_last_value*dt #the area under line
+            self.spread_total_time += dt
+            self.spread_last_time = t
+
+        if ba is None or bb is None:
+            self.spread_check = False
+        else:
+            self.spread = ba.price - bb.price
+            self.spread_last_value = self.spread
+            self.spread_last_time = t
+            self.spread_check = True
+
+
 
     def matching(self, sim):
         global count, T
@@ -85,7 +106,7 @@ class LOB:
             besta = self.best_ask()
             if bestb.price >=besta.price:
                 t = sim.current_time
-                self.spread_status(t)
+                self.spread_object(t)
                 bid_queue.remove(bestb)
                 ask_queue.remove(besta)
                 sim.cancel(bestb.cancel_event)
@@ -97,12 +118,14 @@ class LOB:
                 self.time.record(t - bestb.arrival_time)
                 self.time.record(t - besta.arrival_time)
                 self.queue_status(t)
-                self.spread_status(t)
+                self.spread_object(t)
                 count += 1
                 if count >= 500:
                     T = t
                     sim.stop()
                     return  #use this to stop the function but not returning anything
+            else:
+                break
 
 class event_arrival(Event):
     def __init__(self,n,time,lob):
@@ -110,6 +133,7 @@ class event_arrival(Event):
         self.cancelled = False
         self.n=n
         self.lob=lob
+        self._seq = 0 #this is for core.py to check
 
     def execute(self, sim):   #I just found that the library could only call event.execute()
         global count, T
@@ -128,10 +152,10 @@ class event_arrival(Event):
             lob.limit_counter.increment() #I hope this increment will work(hope/ cannot use + because it is object
 
 
-            lob.spread_status(t)
+            lob.spread_object(t)
             lob.ba(order)
             lob.queue_status(t)
-            lob.spread_status(t)
+            lob.spread_object(t)
 
             cancel_time = cancellation(n, t)
             cancel_event = event_cancel(order,cancel_time,lob)
@@ -146,7 +170,7 @@ class event_arrival(Event):
                 opposite = lob.best_bid()
 
             if opposite is not None: #in case the market order come without a bb/ba exist
-                lob.spread_status(t)
+                lob.spread_object(t)
 
                 if opposite.binary == "bid":
                     bid_queue.remove(opposite)
@@ -157,7 +181,7 @@ class event_arrival(Event):
                 opposite.status = "matched"
                 lob.time.record(t - opposite.arrival_time)
                 lob.queue_status(t)
-                lob.spread_status(t)
+                lob.spread_object(t)
 
                 count += 1
 
@@ -166,8 +190,9 @@ class event_arrival(Event):
                     sim.stop()
                     return
 
-            if count < 500:
-                sim.schedule(event_arrival(n+1, t+arrival(n), lob)) # schedule for next arrival but arrival is returning a float not class
+        if count < 500:
+            lob.matching(sim)
+            sim.schedule(event_arrival(n+1, t+arrival(n), lob)) # schedule for next arrival but arrival is returning a float not class
 
 
 class event_cancel(Event):
@@ -176,6 +201,7 @@ class event_cancel(Event):
         self.cancelled = False
         self.order = order
         self.lob = lob
+        self._seq = 0
 
     def execute(self,sim):
         global count, T
@@ -186,7 +212,7 @@ class event_cancel(Event):
         if order.matched:
             return
 
-        lob.spread_status(t)
+        lob.spread_object(t)
 
         if order.binary == "bid":
             bid_queue.remove(order)
@@ -198,7 +224,7 @@ class event_cancel(Event):
         lob.cancel_counter.increment()
 
         lob.queue_status(t)
-        lob.spread_status(t)
+        lob.spread_object(t)
         lob.matching(sim)
 
 
@@ -214,21 +240,31 @@ def time_avg_spread(spread):
     a = 0
     b = 0
     prevtime = 0
-    for t in spread.keys:
-        if spread[t] == "a":   # updates the time but does not add anything so we are not counting one spred for longer then we should because there was none
+    for t in spread.keys(): #changed a bit grammar
+        if spread[t] is None:   # updates the time but does not add anything so we are not counting one spred for longer then we should because there was none
             prevtime = t
         else:
             a += (t - prevtime)*spread[t]
             b +=  (t - prevtime)
             prevtime = t
-    t_avg_spread = a/b
+    if b !=0:
+        t_avg_spread = a/b
     return t_avg_spread
 
 
 
 
 if __name__ == "__main__":
-    simulation()
+    lob,sim = simulation()
 
-    total_limit = LOB.limit_counter.value
-    cancelled = LOB.cancel_counter.value
+    total_limit = lob.limit_counter.value
+    cancelled = lob.cancel_counter.value
+
+    #avg_spread = time_avg_spread()
+    if lob.spread_total_time > 0:
+        avg_spread = lob.spread_integral / lob.spread_total_time
+    else:
+        avg_spread = 0
+    print(f"T:{T}, total:{total_limit} , matched:{total_limit - cancelled}, cancelled:{cancelled}"
+          f"num of trades:{count}, time avg spread{avg_spread}, avg time{lob.time.mean()}, cancellation rate:{lob.cancel_counter.fraction(total_limit)}"
+          f"avg bid queue length{lob.bid_length.mean(T)}, ask:{lob.ask_length.mean(T)}")
