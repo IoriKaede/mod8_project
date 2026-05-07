bc34ff7d208d0e325a18a1e5a9dcdfd3205325e9
IoriKaede <paranoia170214@gmail.com>
2026-05-07 15:30:25 +0200

    update arrival

--- Assignment2/solution.py
+++ Assignment2/solution.py
@@ -13,13 +13,38 @@ x = np.random.exponential(scale=...)
 # Rerouting threshold: K = 5
 #metrics:Waiting time.Queue length.Truck utilisation.
 reroute_count = 0
+
+
+l_1 ,l_2 ,l_3 = 0.4, 0.4, 0.4
+q_1,q_2,q_3 = 1/3, 1/3, 1/3
+k_1 = 2
+mu_1 = 1.0
+k_2 = 3
+mu_2 = 1.5
+mu_3 = 1.0
+p = 0.5
+K = 5
+random.seed(42)
+
+
+
+
+def Erlang(k,mu):
+    l = []
+    count = 0
+    while count in range(k):
+        l.append(np.random.exponential(mu))
+        count +=1
+    return sum(l)
+
+
 class UWC:
     def __init__(self,i):
         self.truck = i
         self.distinct = i
         self.home_district = [1,2,3] #range of i also below is
-        self.waste = [1,2,3]
-        self.queues = [0,0]  #arrival time, waste type
+        self.type = 0
+        self.queues = [[],[],[]]  #arrival time, waste type
         self.truck_status = "idle"  #idle,serving_home,serving_foreign
         self.truck_serving = None  # check if currently serving
         self.counter = 0
@@ -31,9 +56,22 @@ class UWC:
 
     def arrival(self):
         i = self.distinct
+        queue = self.queues
         #schedule next arrival = t+exp(λi)
+        #arrival(time) to some distinct
+        rd = random.random()
+        if rd <= q_1:
+            self.type = 1
+            self.service_time = Erlang(k_1, mu_1)
+        elif rd <= q_1+q_2:
+            self.type = 2
+            self.service_time = Erlang(k_2, mu_2)
+        else:
+            self.type = 3
+            self.service_time = np.random.exponential(mu_3)
+
         #random waste type and append (t, type) to queues
-        if self.truck_status =="idel":
+        if self.truck_status =="idle":
             #service(truck=i, district=i)
         if self.truck_status =="serving foreign":
             #check rerouting condition
